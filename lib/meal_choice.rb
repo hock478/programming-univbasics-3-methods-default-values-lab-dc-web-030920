b231c6b1ab5a8c8ce9e6260fe224da57ae851d62
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-02 09:18:04 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(veg1,veg2, protein = "meat")
-  puts "What a nutritious meal!".inspect
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  return 0 
+  puts "What a nutritious meal!"
+  message = "A plate of #{protein} with #{veg1} and #{veg2}."
+  return 0
 end
